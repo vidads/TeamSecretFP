a1f24f7a958fc8f7142f83eb41351bd968ba6072
chongkai <chongkai96@gmail.com>
2021-04-23 10:46:18 +0800

    Feedback javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
 // For Contact Us
 
+var feedbackList = [];
 
+function submitFeedback() {
+  //Pass the input values to variables
 
+  var uName = document.querySelector("#user_name").value;
+  var uEmail = document.querySelector("#user_email").value;
+  var uFback = document.querySelector("#fback_text").value;
 
+  addFeedback(uName, uEmail, uFback);
+}
 
+function addFeedback(uName, uEmail, uFback) {
+  var time = new Date(),
+    feedbackTime = `${time.getDate()}/${
+      time.getMonth() + 1
+    }/${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}`;
 
+  const item = {
+    i_name: uName,
+    i_email: uEmail,
+    i_fback: uFback,
+    i_time: feedbackTime,
+  };
+
+  feedbackList.push(item);
+
+  listFeedback();
+}
+
+function listFeedback() {
+  console.log(`Total number of Feedbacks ${feedbackList.length}`);
+  console.log(feedbackList);
+}
 // End for Contact Us
