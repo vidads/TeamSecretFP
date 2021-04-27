3eb013dd3f7859c54c065955ef6441628ca72c15
chongkai <chongkai96@gmail.com>
2021-04-27 17:04:59 +0800

    new about us

---
+++ js/feedback.js
@@ -0,0 +1,68 @@
+// For Contact Us
+
+var feedbackList = [];
+
+function submitFeedback() {
+  //Pass the input values to variables
+
+  var uName = document.querySelector("#user_name").value;
+  var uEmail = document.querySelector("#user_email").value;
+  var uFback = document.querySelector("#fback_text").value;
+
+  addFeedback(uName, uEmail, uFback);
+
+  //Closing and clearing the modal after feedback is submitted
+
+  $("#feedbackModal").modal("hide");
+
+  $("#feedbackModal").on("hidden.bs.modal", function () {
+    $("#feedbackForm")
+      .find('input[type="text"], input[type="email"], textarea')
+      .val("");
+  });
+}
+
+function addFeedback(uName, uEmail, uFback) {
+  var time = new Date(),
+    day = time.getDate(),
+    month = time.getMonth() + 1,
+    year = time.getFullYear(),
+    hour = time.getHours(),
+    minutes = time.getMinutes();
+
+  if (day < 10) {
+    day = "0" + day;
+  }
+
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  var feedbackTime =
+    day + "/" + month + "/" + year + " " + hour + ":" + minutes;
+
+  const item = {
+    i_name: uName,
+    i_email: uEmail,
+    i_fback: uFback,
+    i_time: feedbackTime,
+  };
+
+  feedbackList.push(item);
+
+  listFeedback();
+}
+
+function listFeedback() {
+  console.log(`Total number of Feedbacks ${feedbackList.length}`);
+  console.log(feedbackList);
+}
+// End for Contact Us
