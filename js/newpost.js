3be7b27fa060f4cc7ace7956ad55a7bc55e5b594
vidads <vidadse@yahoo.com>
2021-04-29 03:09:19 +0800

    form validation done

--- js/newpost.js
+++ js/newpost.js
@@ -14,11 +14,14 @@ function saveinfo() {
     document.getElementById("gender2").src = "IMG/protection-mask-male.png";
   } else
     document.getElementById("gender2").src = "IMG/protection-mask-female.png";
+
+  $("#myModal").modal("hide");
 }
 
 // onload form for username and gender
 $(window).on("load", function () {
   $("#myModal").modal("show");
+  localStorage.clear();
 });
 
 // load view image to be posted
@@ -26,3 +29,39 @@ var loadFile = function (event) {
   var output = document.getElementById("load2");
   output.src = URL.createObjectURL(event.target.files[0]);
 };
+
+// For Username and gender form validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+// (function () {
+//   "use strict";
+//   window.addEventListener(
+//     "load",
+//     function () {
+//       // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//       var forms = document.getElementsByClassName("needs-validation");
+//       // Loop over them and prevent submission
+//       var validation = Array.prototype.filter.call(forms, function (form) {
+//         form.addEventListener(
+//           "button",
+//           function (event) {
+//             if (form.checkValidity() === false) {
+//               event.preventDefault();
+//               event.stopPropagation();
+//             }
+//             form.classList.add("was-validated");
+//             $("#modalName").modal("hide");
+//           },
+//           false
+//         );
+//       });
+//     },
+//     false
+//   );
+// })();
+//End for Username and gender form validation
+// $("#myModal").on("hide.bs.modal", function (e) {
+//   // Submit data to server and in success callback write if to validate whether form data is valid or not .
+//   if (invalidForm) {
+//     e.preventDefault();
+//   }
+// });
